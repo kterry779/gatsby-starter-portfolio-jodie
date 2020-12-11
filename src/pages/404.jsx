900627ff0aef76de04de02a22a353f3cc5538590
kterry779 <kterry779@gmail.com>
2020-12-11 10:44:46 -0700

    Add gif to files
    add 404 gif to files and place in 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -6,7 +6,7 @@ const NotFound = () => (
     <div style={{ textAlign: `center` }}>
       <h1>404</h1>
       <p>Page not found.</p>
-      <img src="http://gph.is/28QnwLE" alt="gif">
+      <img src="404.gif" alt="gif">
     </div>
   </Layout>
 )
